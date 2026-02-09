841a34e6eecdc901105cd62867e61d266bed565f
QinYuyang <1430659802@qq.com>
2026-02-09 15:43:34 +0800

    [Dev] Add Create TouchList Option

---
+++ My project/Assets/Editor/YGUIMenuOptions.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.Design;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using MenuCommand = UnityEditor.MenuCommand;
+
+public static class YGUIMenuOptions
+{
+    [MenuItem("GameObject/YGUI/TouchList", false, 2027)]
+    public static void AddTouchList(MenuCommand menuCommand)
+    {
+        GameObject go = new GameObject("TouchList", typeof(RectTransform));
+        go.AddComponent<YTouchList>();
+
+        go.transform.SetParent(FindFirstCanvas(menuCommand).transform, false);
+
+        Undo.RegisterCreatedObjectUndo(go, "Create TouchList");
+        Selection.activeGameObject = go;
+    }
+
+    private static GameObject CreateCanvas()
+    {
+        GameObject canvasGO = ObjectFactory.CreateGameObject("Canvas");
+        Canvas canvas = ObjectFactory.AddComponent<Canvas>(canvasGO);
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        ObjectFactory.AddComponent<CanvasScaler>(canvasGO);
+        ObjectFactory.AddComponent<GraphicRaycaster>(canvasGO);
+
+        if (Object.FindAnyObjectByType<EventSystem>(FindObjectsInactive.Include) == null)
+        {
+            GameObject esGO = ObjectFactory.CreateGameObject("EventSystem");
+            ObjectFactory.AddComponent<EventSystem>(esGO);
+            ObjectFactory.AddComponent<StandaloneInputModule>(esGO);
+        }
+
+        return canvasGO;
+    }
+
+    private static GameObject FindFirstCanvas(MenuCommand menuCommand)
+    {
+        GameObject go = menuCommand.context as GameObject;
+        if (go == null || go.GetComponentInParent<Canvas>() == null)
+        {
+            go = GameObject.FindFirstObjectByType<Canvas>()?.gameObject ?? CreateCanvas();
+        }
+        return go;
+    }
+}
---
+++ My project/Assets/Scripts/Element/YTouchList.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[AddComponentMenu("YGUI/Touch List", 38)]
+[SelectionBase]
+[ExecuteAlways]
+[DisallowMultipleComponent]
+[RequireComponent(typeof(RectTransform))]
+public class YTouchList : UIBehaviour
+{
+    
+}
